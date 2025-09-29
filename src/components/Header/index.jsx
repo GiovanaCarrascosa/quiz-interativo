e5c3c27d643b0fd31c4806495bb48d3f540982d2
Giovana Carrascosa <giovanaa.carrascosa194@gmail.com>
2025-09-29 10:00:06 -0300

    ATT: header funcionando

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,65 @@
 // importação do css do header
-import './Header.module.css';
+import styles from './Header.module.css';
 import React, { useState, useEffect } from 'react';
 
 // função do header
 function Header() {
 
-    
+  // cria uma variável 'segundos' e uma função pra mudar ela, começando em 0
+  const [segundos, setSegundos] = useState(0);
+
+  // cria uma variável 'minutos' e uma função pra mudar ela, também começando em 0
+  const [minutos, setMinutos] = useState(0);
+
+  useEffect(() => {
+
+    // executa algo a cada 1000 milissegundos
+    const intervalo = setInterval(() => {
+
+      // atualiza o estado dos segundos
+      setSegundos(segundosAnteriores => {
+
+        // se os segundos chegam em 59
+        if (segundosAnteriores === 59) {
+
+          // a gente atualiza os minutos, somando mais um
+          setMinutos(minutosAnteriores => minutosAnteriores + 1);
+
+          // e reseta os segundos pra 0
+          return 0;
+
+        } else {
+
+          // se não, a gente soma mais um segundo
+          return segundosAnteriores + 1;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(intervalo);
+
+    // o array vazio faz com que esse efeito rode só uma vez, quando a página é carregada
+  }, []); 
+
+  // formata os segundos pra ter sempre 2 dígitos
+  const segundosFormatados = String(segundos).padStart(2, '0');
+
+  // faz a mesma coisa pra formatar os minutos
+  const minutosFormatados = String(minutos).padStart(2, '0');
+  
   return (
   
-    <header className="app-header">
-      <h1 className="header-title">Quiz Interativo</h1>
-      <span>Questão 1 de 10</span>
+    <header className={styles.appHeader}>
 
-      <p></p>
+      {/* titulo do header */}
+      <h1 className={styles.headerTitle}>Quiz Interativo! 🧠</h1>
+
+      {/* paragrafo normal com um aviso de questões */}
+      <h3 className={styles.paragrafo}>Questão 1 de 10.</h3>
+
+      {/* aqui ele chama os segundos e minutos formatados */}
+      <h2 className={styles.tempo}>Tempo: {minutosFormatados}:{segundosFormatados}</h2>
+
     </header>
   );
 }
